a8a3ac0211b96cffffe449d718d4aca8e4515d7d
Wander Junior <wander.junior@infracommerce.com.br>
2018-09-03 17:17:05 -0300

    AGORA VAI

--- features/step_definitions/busca.rb
+++ features/step_definitions/busca.rb
@@ -1,5 +1,5 @@
 Quando("eu realizar busca por {string}") do |texto|
-  page.find('#sb_ifc0 input:nth-child(1)').set texto
+  page.find('input#lst-ib').set texto
   page.find('#sbtc > div.gstl_0.sbdd_a > div:nth-child(2) > div.sbdd_b > div > ul > li:nth-child(11) > div > span:nth-child(1) > span > input').click
 end
 
