df7c1c3488b5f8a3fe54f7387ee7bf64b92b18a6
Wander Junior <wander.junior@infracommerce.com.br>
2018-09-03 17:06:56 -0300

    AGORA VAI

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,7 +17,7 @@ end
 
 Capybara.register_driver :chrome do |app|
 	options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
-	Selenium::WebDriver::Chrome.driver_path = './drivers/chromedriver'
+	Selenium::WebDriver::Chrome.driver_path = './drivers/chromedriver.exe'
 	Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
